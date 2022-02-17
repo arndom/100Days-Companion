f8c296bf707307030f7eb2385e1d943c66349031
arndom <aminonimisi2@gmail.com>
2022-02-17 23:56:29 +0100

    lang api

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,12 @@
+import { fetchTopLanguages } from './utils';
 import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
+// // // Start writing Firebase Functions
+// // // https://firebase.google.com/docs/functions/typescript
+// //
+export const helloWorld = functions.https.onRequest(async (request, response) => {
   functions.logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
+  const getLanguage = await fetchTopLanguages('ardnom');
+  // console.log('Your top language is', Object.keys(getLanguage)[0]);
+  response.send(getLanguage);
 });
