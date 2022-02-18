28a5b9e9e63d7b29001928a3c55dbb8b113f1f27
Omeiza Alabi <omeizaalabi@gmail.com>
2022-02-18 17:32:37 +0100

    first phase of design

--- src/pages/PublicRoadmap/PublicRoadmap.tsx
+++ src/pages/PublicRoadmap/PublicRoadmap.tsx
@@ -1,55 +1,131 @@
-/* eslint-disable prettier/prettier */
-import { Box, Card, CardContent, Typography, List, ListItem, ListItemText, Divider, IconButton } from '@mui/material';
-import { KeyboardArrowUp } from '@mui/icons-material';
+import {
+  Box,
+  Card,
+  Grid,
+  CardContent,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  Divider,
+  IconButton,
+  InputBase,
+  Button,
+} from '@mui/material';
+import { KeyboardArrowUp, SearchRounded } from '@mui/icons-material';
 import { useStyles } from './useStyles';
 
-
 interface IRoadmapStatusCard {
-    title: string;
-    titleBackground: string;
-    options: { title: string, type: string, votes: number }[];
+  title: string;
+  color: string;
+  options: { title: string; type: string; votes: number }[];
 }
 
-const RoadmapStatusCard = ({ title, titleBackground, options }: IRoadmapStatusCard) => {
-    const classes = useStyles();
-    return (
-        <Card sx={{ width: 300, height: 400, background: '#292950' }}>
-            <CardContent>
-                <Typography variant="h6" sx={{ color: '#fff', marginBottom: '10px' }}>
-                    <span style={{ background: `${titleBackground}` }} className={classes.title}>{title.toUpperCase()}</span>
-                </Typography>
-                <List sx={{ width: '100%', maxWidth: 360 }}>
-                    {options.map((option, index) => (
-                        <ListItem key={index} sx={{ paddingLeft: 0 }} secondaryAction={
-                            <IconButton aria-label="more details" sx={{ marginRight: '10px' }}>
-                                <Box>
-                                    <KeyboardArrowUp sx={{ color: '#9020fb' }} />
-                                    <Typography sx={{ color: '#fff' }}>{option.votes}</Typography>
-                                </Box>
-                            </IconButton>
-                        }>
-                            <Divider orientation='vertical' sx={{ height: '4rem', borderLeft: '3px solid grey', marginRight: '7px' }} />
-                            <ListItemText className={classes.listItemText} primary={option.title} secondary={option.type} secondaryTypographyProps={{ color: '#fff', marginTop: '2px' }} />
-                        </ListItem>
-                    ))}
-                </List>
-            </CardContent>
-        </Card>
-    );
-}
+const RoadmapStatusCard = ({ title, color, options }: IRoadmapStatusCard) => {
+  const classes = useStyles();
+  return (
+    <Card sx={{ width: 415, height: 400, background: '#292950' }}>
+      <CardContent>
+        <Typography variant="h6" sx={{ color: '#fff', marginBottom: '10px' }}>
+          <span style={{ background: `${color}` }} className={classes.title}>
+            {title.toUpperCase()}
+          </span>
+        </Typography>
+        <List sx={{ width: '100%', maxWidth: 360 }}>
+          {options.map((option, index) => (
+            <ListItem
+              key={index}
+              sx={{ paddingLeft: 0 }}
+              secondaryAction={
+                <IconButton aria-label="more details">
+                  <Box>
+                    <KeyboardArrowUp sx={{ color: '#9020fb' }} />
+                    <Typography sx={{ color: '#fff' }}>{option.votes}</Typography>
+                  </Box>
+                </IconButton>
+              }
+            >
+              <Divider
+                orientation="vertical"
+                sx={{ height: '4rem', borderLeft: `3px solid ${color}`, marginRight: '7px' }}
+              />
+              <ListItemText
+                className={classes.listItemText}
+                primary={option.title}
+                secondary={option.type}
+                secondaryTypographyProps={{ color: '#fff', marginTop: '2px' }}
+              />
+            </ListItem>
+          ))}
+        </List>
+      </CardContent>
+    </Card>
+  );
+};
 
 const PublicRoadmap = () => {
-    const classes = useStyles();
-    const suggestions = [{ title: 'Courses', type: 'Feature request', votes: 10 }, { title: 'Courses', type: 'Feature request', votes: 10 }];
-    const planned = [{ title: 'Courses', type: 'Feature request', votes: 10 }];
-    const inProgress = [{ title: 'Courses', type: 'Feature request', votes: 10 }];
-    const live = [{ title: 'Courses', type: 'Feature request', votes: 10 }];
+  const classes = useStyles();
 
-    return (
-        <Box>
-            <RoadmapStatusCard title="Suggestions" titleBackground="#9020fb" options={suggestions} />
-        </Box>
-    );
+  const suggestions = [
+    { title: 'Courses', type: 'Feature request', votes: 10 },
+    { title: 'Courses', type: 'Feature request', votes: 10 },
+  ];
+  const planned = [{ title: 'Courses', type: 'Feature request', votes: 10 }];
+  const inProgress = [{ title: 'Courses', type: 'Feature request', votes: 10 }];
+  const live = [{ title: 'Courses', type: 'Feature request', votes: 10 }];
+
+  return (
+    <Grid container className={classes.root}>
+      <Grid item xs={6}>
+        Logo
+      </Grid>
+      <Grid item xs={6}>
+        <Button variant="outlined" className={classes.home} disableRipple>
+          <Typography>Home</Typography>
+        </Button>
+      </Grid>
+      <Grid container gap={4}>
+        <Grid sx={{ marginTop: '20px' }} container xs={12} direction="row" justifyContent="flex-end">
+          <IconButton aria-label="search" sx={{ marginBottom: '6px' }}>
+            <InputBase sx={{ color: '#fff' }} placeholder="Search" />
+            <SearchRounded sx={{ color: '#fff', marginRight: '3px' }} />
+          </IconButton>
+        </Grid>
+        <Grid item xs={12}>
+          <Card sx={{ maxWidth: 415, height: 50, background: '#292950' }}>
+            <CardContent>
+              <Grid container>
+                <Grid item xs={6}>
+                  <Typography sx={{ color: '#fff' }}>Feature requests</Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography sx={{ color: '#fff' }}>
+                    <span style={{ background: '#f26e3f' }} className={classes.title}>
+                      58
+                    </span>
+                  </Typography>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item container spacing={4} xs={12}>
+          <Grid item>
+            <RoadmapStatusCard title="Suggestions" color="#9020fb" options={suggestions} />
+          </Grid>
+          <Grid item>
+            <RoadmapStatusCard title="Planned" color="#1B3C40" options={planned} />
+          </Grid>
+          <Grid item>
+            <RoadmapStatusCard title="In Progress" color="#3E3418" options={inProgress} />
+          </Grid>
+          <Grid item>
+            <RoadmapStatusCard title="Live" color="#223E2B" options={live} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default PublicRoadmap;
--- src/pages/PublicRoadmap/useStyles.ts
+++ src/pages/PublicRoadmap/useStyles.ts
@@ -2,6 +2,48 @@ import { Theme } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 
 export const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    padding: theme.spacing(2, 12),
+    backgroundColor: '#0e0219',
+    minHeight: '100vh',
+    color: '#fff',
+
+    [theme.breakpoints.down('md')]: {
+      padding: theme.spacing(2, 6),
+    },
+
+    [theme.breakpoints.down('sm')]: {
+      padding: theme.spacing(2, 4),
+    },
+  },
+
+  home: {
+    '&.MuiButton-root': {
+      background: 'linear-gradient(to right, #F26E3F, #9020fb)',
+      border: 'none',
+      borderRadius: '30px',
+      padding: '3px',
+      display: 'block',
+      marginLeft: 'auto',
+
+      '&:hover': {
+        border: 'none',
+      },
+    },
+
+    '&>.MuiTypography-root': {
+      borderRadius: '30px',
+      background: '#0e0219',
+      width: '100%',
+      padding: theme.spacing(1, 3),
+      textTransform: 'none',
+      fontSize: '1rem',
+      color: '#fff',
+    },
+  },
+
+  roadmap: { width: 415, height: 400, background: '#292950' },
+
   title: {
     fontSize: '0.7rem',
     padding: '0.45em 1em',
@@ -11,5 +53,4 @@ export const useStyles = makeStyles((theme: Theme) => ({
   listItemText: {
     color: '#fff',
   },
-
 }));
