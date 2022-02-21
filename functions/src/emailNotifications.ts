4e8e49883600a5b5d7944c9d9f76d3aaeb9d1173
Omeiza Alabi <omeizaalabi@gmail.com>
2022-02-21 13:01:27 +0100

    add getUsers function, and send mail functions

--- functions/src/emailNotifications.ts
+++ functions/src/emailNotifications.ts
@@ -1,15 +1,25 @@
 'use strict';
-import nodemailer = require('nodemailer');
+import * as nodemailer from 'nodemailer';
+// import * as cron from 'node-cron';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
+
+interface Transport {
+  host: string;
+  service: string | undefined;
+  port: number;
+  auth: { user: string | undefined; pass: string | undefined };
+}
 
 interface MailOptions {
   from: string;
   to: string;
   subject: string;
   text: string;
-  html: string;
 }
 
-const transporter = nodemailer.createTransport({
+const transport: Transport = {
   host: 'gmail',
   service: process.env.EMAIL_SERVICE,
   port: 2525,
@@ -17,12 +27,69 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-});
+};
 
-const sendMail = async (mailOptions: MailOptions): Promise<any> => {
+const transporter = nodemailer.createTransport(transport);
+
+const getUsers = async (notificationFrequency: string) => {
+  const users = await admin
+    .firestore()
+    .collection('users')
+    .where('notificationFrequency', '==', notificationFrequency)
+    .get();
+  const usersArray = users.docs.map((doc) => {
+    return doc.data();
+  });
+  return usersArray;
+};
+
+export const sendMail = async (notificationFrequency: string) => {
   try {
-    return await transporter.sendMail(mailOptions);
+    const users = await getUsers(notificationFrequency);
+    users.forEach(async (user) => {
+      const mailOptions: MailOptions = {
+        from: '100 Days Companion',
+        subject: 'Your daily reminder',
+        text: `Hi ${user.username},\n\nThis is your daily reminder.\n\nYou can check your progress here: https://100dayscompanion.com/users/${user.username}\n`,
+        to: '',
+      };
+      mailOptions.to = user.email;
+      return transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+    });
   } catch (error) {
     return error;
   }
 };
+
+// Get users with weekly notifications with firebase subscription
+
+// Cron job to send emails for daily notifications
+// const dailyEmailCron = cron.schedule(
+//   '0 1 * * *',
+//   () => {
+//     console.log('Running a job at 01:00 AM');
+//   },
+//   {
+//     scheduled: true,
+//     timezone: 'Africa/Lagos',
+//   },
+// );
+
+// // Cron job to send emails for weekly notifications
+
+// const weeklyEmailCron = cron.schedule(
+//   '0 1 * * Sun',
+//   () => {
+//     console.log('Running a job every sunday by 01:00 AM');
+//   },
+//   {
+//     scheduled: true,
+//     timezone: 'Africa/Lagos',
+//   },
+// );
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import { fetchTopLanguages } from './fetchTopLanguages';
+import { sendMail } from './emailNotifications';
 import * as functions from 'firebase-functions';
 
 // // // Start writing Firebase Functions
@@ -10,4 +11,12 @@ export const getTopLanguages = functions.https.onRequest(async (request, respons
   const username = request.query.username;
   const getLanguage = await fetchTopLanguages(`${username}`);
   response.send(getLanguage);
+});
+
+export const sendEmails = functions.https.onRequest(async (request, response) => {
+  const frequency = request.query.frequency;
+  functions.logger.info(`Sending ${frequency} mails...`, { structuredData: true });
+
+  const sendMails = await sendMail(`${frequency}`);
+  response.send(sendMails);
 });
