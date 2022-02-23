d1c660014a784bd4147b9ffa86d38689d765cc5d
Omeiza Alabi <omeizaalabi@gmail.com>
2022-02-23 13:11:47 +0100

    add top languages and badges section

--- src/pages/Home/Statistics/Statistics.tsx
+++ src/pages/Home/Statistics/Statistics.tsx
@@ -1,8 +1,8 @@
-import { Grid } from '@mui/material';
+import { Grid, Box, Typography, List, ListItem, ListItemText } from '@mui/material';
 import { ResponsiveContainer, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
-// import { useStyles } from './useStyles';
+import { useStyles } from './useStyles';
 
-// const classes = useStyles;
+const classes = useStyles;
 
 interface ChartProps {
   data: any[];
@@ -10,7 +10,7 @@ interface ChartProps {
 
 const Chart = (props: ChartProps) => (
   <ResponsiveContainer width="100%" height={300}>
-    <LineChart data={props.data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+    <LineChart data={props.data} margin={{ top: 5, right: 55, bottom: 5, left: 0 }}>
       <Line type="monotone" dataKey="frequency" stroke="#8884d8" />
       <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
       <XAxis dataKey="name" />
@@ -21,6 +21,22 @@ const Chart = (props: ChartProps) => (
 );
 
 const Statistics = (): JSX.Element => {
+  const earnedBadges = [
+    { name: '1 Month', image: '../../../assets/images/gem1.png' },
+    { name: '21 Days', image: '../../../assets/images/gem2.png' },
+    { name: '2 Months', image: '../../../assets/images/gem3.png' },
+    { name: '7 Days', image: '../../../assets/images/gem4.png' },
+    { name: 'Capstone', image: '../../../assets/images/gem5.png' },
+  ];
+
+  const topLanguages = [
+    { name: 'JavaScript', image: '../../../assets/images/gem1.png' },
+    { name: 'TypeScript', image: '../../../assets/images/gem2.png' },
+    { name: 'React', image: '../../../assets/images/gem3.png' },
+    { name: 'NodeJs', image: '../../../assets/images/gem4.png' },
+    { name: 'Angular', image: '../../../assets/images/gem5.png' },
+  ];
+
   const frequencyData = [
     { name: 'Day 1', frequency: 20 },
     { name: 'Day 2', frequency: 10 },
@@ -32,9 +48,38 @@ const Statistics = (): JSX.Element => {
   ];
 
   return (
-    <Grid container spacing={3}>
-      <Grid item xs={12} sm={12} md={6}>
-        <Chart data={frequencyData} />
+    <Grid sx={classes.root} container spacing={5}>
+      <Grid container item>
+        <Grid item xs={12} sm={12} md={6}>
+          <Chart data={frequencyData} />
+        </Grid>
+        <Grid item xs={12} sm={12} md={6}>
+          <Box sx={classes.calendar}></Box>
+        </Grid>
+      </Grid>
+      <Grid container item>
+        <Grid sx={classes.badges} item xs={12} sm={12} md={6}>
+          <Typography>Earned badges</Typography>
+          <List>
+            {earnedBadges.map((badge, index) => (
+              <ListItem key={index}>
+                <img src={badge.image} alt="" />
+                <ListItemText primary={badge.name} />
+              </ListItem>
+            ))}
+          </List>
+        </Grid>
+        <Grid sx={classes.languages} item xs={12} sm={12} md={6}>
+          <Typography>Top Languages</Typography>
+          <List>
+            {topLanguages.map((language, index) => (
+              <ListItem key={index}>
+                <img src={language.image} alt="" />
+                <ListItemText primary={language.name} />
+              </ListItem>
+            ))}
+          </List>
+        </Grid>
       </Grid>
     </Grid>
   );
--- src/pages/Home/Statistics/useStyles.tsx
+++ src/pages/Home/Statistics/useStyles.tsx
@@ -1,5 +1,24 @@
+import { theme } from '../../../themes/theme';
 export const useStyles = {
-  chart: {
-    maxWidth: '400px',
+  root: {
+    paddingLeft: '12em',
+    [theme.breakpoints.down('md')]: {
+      paddingLeft: '0',
+    },
+  },
+
+  calendar: {
+    border: '1px solid #ccc',
+    height: '300px',
+    margin: theme.spacing(0, 10),
+  },
+
+  badges: {
+    marginLeft: '40px',
+    width: '200px',
+  },
+
+  languages: {
+    marginLeft: '2px',
   },
 };
